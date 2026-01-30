f2b62896be7c8147bd76b0b75d7ebd5e80e9d39c
Luiz <Luizog235@gmail.com>
2026-01-29 22:35:41 -0300

    create context database

---
+++ Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteConhecimento.Infrastructure.Mapping;
+using TesteConhecimento.Models;
+
+
+namespace TesteConhecimento.Infrastructure.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySql(
+                "server=localhost;database=teste_cadastros;user=MySQLUser;password=MySqlPassword;",
+                ServerVersion.AutoDetect("server=localhost;database=TesteConhecimento;user=MySQLUser;password=MySqlPassword;")
+            );
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>(new PessoaMapping().Configure);
+        }
+    }
+}
---
+++ Infrastructure/Mapping/PessoaMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TesteConhecimento.Models;
+
+namespace TesteConhecimento.Infrastructure.Mapping
+{
+    public class PessoaMapping : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.ToTable("pessoas");
+
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Nome).HasMaxLength(100);
+            builder.Property(e => e.Telefone).HasMaxLength(100);
+        }
+    }
+}
