868f91d4af8950062c72413211718bfb4283fc71
Luiz <Luizog235@gmail.com>
2026-01-30 10:01:00 -0300

    create model pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteConhecimento.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; } // Recomendo Guid invés de ID com auto incremento para evitar colisões em sistemas distribuídos.
+        public string Nome { get ; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+    }
+}
