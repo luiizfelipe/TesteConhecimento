f7f7d5482c75af26c221abf77e76d79864ae9598
Luiz <Luizog235@gmail.com>
2026-01-30 10:00:10 -0300

    create var ConnectiionString

--- Infrastructure/Context/DatabaseContext.cs
+++ Infrastructure/Context/DatabaseContext.cs
@@ -9,10 +9,12 @@ namespace TesteConhecimento.Infrastructure.Context
     {
         public DbSet<Pessoa> Pessoa { get; set; }
 
+        private string ConnectionString = "server=localhost;database=teste_cadastros;user=MySQLUser;password=MySqlPassword;";
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseMySql(
-                "server=localhost;database=teste_cadastros;user=MySQLUser;password=MySqlPassword;",
-                ServerVersion.AutoDetect("server=localhost;database=TesteConhecimento;user=MySQLUser;password=MySqlPassword;")
+                ConnectionString,
+                ServerVersion.AutoDetect(ConnectionString)
             );
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
