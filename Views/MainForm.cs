550b82a255a53ccdf209770371040c34935ba7ac
Luiz <Luizog235@gmail.com>
2026-01-29 22:58:41 -0300

    create function to check connection DataBase

--- Views/MainForm.cs
+++ Views/MainForm.cs
@@ -1,3 +1,5 @@
+using TesteConhecimento.Infrastructure.Context;
+
 namespace TesteConhecimento
 {
     public partial class MainForm : Form
@@ -5,6 +7,29 @@ namespace TesteConhecimento
         public MainForm()
         {
             InitializeComponent();
+            this.Shown += async (s, e) => _ = CheckConnectionDatabase();
+
+        }
+
+        internal async Task CheckConnectionDatabase()
+        {
+            try
+            {
+                using var context = new DatabaseContext();
+                bool connected = await context.Database.CanConnectAsync();
+                if (!connected)
+                {
+                    MessageBox.Show("Não foi possível conectar ao banco de dados.", "Erro de Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                await context.Database.EnsureCreatedAsync(); // Garante que o banco de dados seja criado se não existir.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao conectar com o banco de dados: {ex.Message}", "Erro de Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
